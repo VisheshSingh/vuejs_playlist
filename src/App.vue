f8da64770b22c74decf9524f43173e7076d2fd0a
Vishesh <visheshsingh16@gmail.com>
2018-09-09 13:48:09 -0500

    Creating Dynamic components

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <!-- <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
     <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
     <app-footer v-bind:title="title"></app-footer> -->
-    <form-helper>
+    <!-- <form-helper>
       <div slot="form-header">
         <h3>This is the title of form</h3>
         <p>Information about form</p>
@@ -15,7 +15,12 @@
       <div slot="form-controls">
         <button v-on:click="handleSubmit">Submit</button>
       </div> 
-    </form-helper>
+    </form-helper> -->
+    <keep-alive>
+      <component v-bind:is="component"></component>
+    </keep-alive>
+    <button v-on:click="component = 'form-one'">Show Form one</button>
+    <button v-on:click="component = 'form-two'">Show Form two</button>
   </div>
 </template>
 
@@ -23,18 +28,23 @@
 // import Header from "./components/Header.vue";
 // import Footer from "./components/Footer.vue";
 // import Ninjas from "./components/Ninjas.vue";
-import formHelper from "./components/formHelper.vue";
+// import formHelper from "./components/formHelper.vue";
+import formOne from "./components/formOne.vue";
+import formTwo from "./components/formTwo.vue";
 
 export default {
   components: {
     // "app-header": Header,
     // "app-ninjas": Ninjas,
     // "app-footer": Footer,
-    "form-helper": formHelper
+    // "form-helper": formHelper,
+    "form-one": formOne,
+    "form-two": formTwo
   },
   data() {
     return {
       // title: "Vue ninjas",
+      component: "form-two",
       ninjas: [
         { name: "Ryu", speciality: "Vue Components", show: false },
         { name: "Crystal", speciality: "HTML Wizardry", show: false },
