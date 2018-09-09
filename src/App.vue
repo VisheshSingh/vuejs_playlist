4a0f0159c0df6f5a5d0c5a263ef3657fb86b4461
Vishesh <visheshsingh16@gmail.com>
2018-09-09 13:25:03 -0500

    The idea of using slots

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,32 @@
 <template>
   <div>
-    <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
+    <!-- <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
     <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
-    <app-footer v-bind:title="title"></app-footer>
+    <app-footer v-bind:title="title"></app-footer> -->
+    <form-helper>
+      <h2 slot="title">{{title}}</h2>
+      <p slot="para">I am a paragraph of slot</p>
+    </form-helper>
   </div>
 </template>
 
 <script>
-import Header from "./components/Header.vue";
-import Footer from "./components/Footer.vue";
-import Ninjas from "./components/Ninjas.vue";
+// import Header from "./components/Header.vue";
+// import Footer from "./components/Footer.vue";
+// import Ninjas from "./components/Ninjas.vue";
+import formHelper from "./components/formHelper.vue";
 
 export default {
   components: {
-    "app-header": Header,
-    "app-ninjas": Ninjas,
-    "app-footer": Footer
+    // "app-header": Header,
+    // "app-ninjas": Ninjas,
+    // "app-footer": Footer,
+    "form-helper": formHelper
   },
   data() {
     return {
-      title: "Vue ninjas",
+      // title: "Vue ninjas",
+      title: "I am a dynamic slot title",
       ninjas: [
         { name: "Ryu", speciality: "Vue Components", show: false },
         { name: "Crystal", speciality: "HTML Wizardry", show: false },
---
+++ src/components/formHelper.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <slot name="title"></slot>
+    <h1>I am a form helper</h1>
+    <slot name="para"></slot>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
