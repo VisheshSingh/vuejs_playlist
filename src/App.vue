f8b75bd691dcea336d43343d047ae1db16fdef31
Vishesh <visheshsingh16@gmail.com>
2018-09-08 23:12:59 -0500

    Initial setup using vue-cli

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ greet() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Vue is fun!'
+    }
+  },
+  methods: {
+    greet() {
+      return 'heyaa cowboys!'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
